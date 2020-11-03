7ad4491a2ab3a40d6756057d554dafbaa7a57432
nsergeev <sergeevkep@icloud.com>
2020-11-03 19:58:22 +1100

    Heat-map Finished

--- Assets/Scripts/Painter.cs
+++ Assets/Scripts/Painter.cs
@@ -8,11 +8,13 @@ using UnityEngine;
 public class Painter : MonoBehaviour
 {
     // definition for HSV
-    // public float Hue = 0;
     public const float Sat = 1;
     public const float Val = 1;
 
-    [Range(0, 10000)]
+    public bool renderTexture;
+    
+    public bool autoThreshold;
+    [Range(2, 10000)]
     public float threshold;
     
     public int resolution = 512;
@@ -23,25 +25,36 @@ public class Painter : MonoBehaviour
     
     private uint[,] _intensityMask;
     private Material _material;
+
+    // Right now these are hardcoded, but I can use local scale from GameObjects and translate them to positions
+    private const float MaxX = 25;
+    private const float MinX = -25;
+    private const float MaxY = 25;
+    private const float MinY = -25;
+
+    private const float W = (MaxX - MinX);
+    private const float H = (MaxY - MinY);
+    private const float HalfW = W / 2;
+    private const float HalfH = H / 2;
     
-    public static float MaxX = 25;
-    public static float MinX = -25;
-    public static float MaxY = 25;
-    public static float MinY = -25;
-    
-    private static float W = (MaxX - MinX);
-    private static float H = (MaxY - MinY);
-    private static float halfW = W/2;
-    private static float halfH = H/2;
-    private static float resW;
-    private static float resH;
+    private static float _resW;
+    private static float _resH;
 
     public List<MonoBehaviour> cPlayers = new List<MonoBehaviour>();
     
+    private Coroutine _saveTextureCoroutine;
+
+    private void OnDisable()
+    {
+        if (_saveTextureCoroutine == null) return;
+        StopCoroutine(_saveTextureCoroutine);
+        Debug.Log("Coroutine was interrupted");
+    }
+
     private void Start()
     {
-        resW = resolution / W;
-        resH = resolution / H;
+        _resW = resolution / W;
+        _resH = resolution / H;
         
         _material = GetComponent<Renderer>().material;
         
@@ -76,46 +89,64 @@ public class Painter : MonoBehaviour
 
     private void FixedUpdate()
     {
-        UpdateIntensityMapValues(cPlayers[0]);
-
-        // { 
-        // PaintUsingPosition(cPlayers[0]);
-        // }
+        foreach (var player in cPlayers)
+        {
+            UpdateIntensityMapValues(player);
+        }
     }
 
-    private void LateUpdate()
+    private void Update()
     {
-        // if (!Input.GetKeyDown(KeyCode.Space)) return;
-        IntensityMapToTexture2D();
-        // SaveTextureAsPNG(_map, "Assets/Textures/Heatmap/Image.png");
+        if (renderTexture)
+        {
+            IntensityMapToTexture2D();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            IntensityMapToTexture2D();
+            if (_saveTextureCoroutine != null)
+            {
+                StopCoroutine(_saveTextureCoroutine);
+            }
+            _saveTextureCoroutine = StartCoroutine(SaveTextureAsPNG(_map,
+                "Assets/Textures/Heatmap/",
+                threshold)
+            );
+        }
     }
 
-    public static void SaveTextureAsPNG(Texture2D texture, string fullPath)
+    private static IEnumerator SaveTextureAsPNG(Texture2D texture, string fullPath, float value)
     {
         var bytes = texture.EncodeToPNG();
-        System.IO.File.WriteAllBytes(fullPath, bytes);
-        Debug.Log(bytes.Length / 1024  + "Kb was saved as: " + fullPath);
-    }
+        if (!fullPath.EndsWith("/"))
+        {
+            fullPath += "/";
+        }
+
+        var filePath = fullPath + $"Heatmap_threshold_{value}.png";
+        System.IO.File.WriteAllBytes(filePath, bytes);
+        Debug.Log(bytes.Length / 1024  + "Kb was saved as: " + filePath);
     
+        yield return null;
+    }
+
     private void UpdateIntensityMapValues(Component player)
     {
         var pos = player.transform.position;
-        var x = Mathf.RoundToInt(Mathf.Abs(pos.x - halfW) * resW);
-        var z = Mathf.RoundToInt(Mathf.Abs(pos.z - halfH) * resH);
+        var x = Mathf.RoundToInt(Mathf.Abs(pos.x - HalfW) * _resW);
+        var z = Mathf.RoundToInt(Mathf.Abs(pos.z - HalfH) * _resH);
 
-        // _intensityMask[x, z] += 1;
-        // ++_intensityMask[x, z];
-        
-        var L = x - closePixels < 0 ? 0 : x - closePixels;
-        var R = x + closePixels > _map.width ? _map.width : x + closePixels;
-        var U = z - closePixels < 0 ? 0 : z - closePixels;
-        var B = z - closePixels > _map.height ? _map.height : z + closePixels;
-        
-        for (var i = L; i < R; ++i)
+        var left  = x - closePixels < 0 ? 0 : x - closePixels;
+        var right = x + closePixels > _map.width ? _map.width : x + closePixels;
+        var top   = z - closePixels < 0 ? 0 : z - closePixels;
+        var bottom= z - closePixels > _map.height ? _map.height : z + closePixels;
+
+        for (var i = left; i < right; ++i)
         {
-            for (var j = U; j < B; ++j)
+            for (var j = top; j < bottom; ++j)
             {
-                var frac = (uint) Mathf.FloorToInt(closePixels / ((Mathf.Abs(i - x) + Mathf.Abs(j - z)) * 0.5f + 3));
+                var frac = (uint) Mathf.FloorToInt(closePixels / ((Mathf.Abs(i - x) + Mathf.Abs(j - z)) * 0.5f + 2.5f));
                 _intensityMask[i, j] += frac;
             }
         }
@@ -123,7 +154,7 @@ public class Painter : MonoBehaviour
     
     private void IntensityMapToTexture2D()
     {
-        if (threshold < 1)
+        if (autoThreshold)
         {
             for (var i = 0; i < _map.height; ++i)
             {
@@ -137,7 +168,6 @@ public class Painter : MonoBehaviour
             }
         }
 
-        // var invMax = threshold > 1 ? 1/threshold : 1;
         var invMax = 1.0f / threshold;
         
         for (var i = 0; i < _map.height; ++i)
@@ -145,11 +175,8 @@ public class Painter : MonoBehaviour
             for (var j = 0; j < _map.width; ++j)
             {
                 var val = Mathf.Clamp(_intensityMask[i, j] * invMax, 0, 1);
-                // var grayscale = val == 0 ?
-                //     _emptyColor : new Color(val, val, val, 1);
-                // _map.SetPixel(i, j, grayscale);
-                
-                var rgb = val == 0 ? Color.clear : HSVToRGB2(val);
+                var rgb = val == 0 ?
+                    Color.clear : HSVToRGB(val);
                 
                 _map.SetPixel(i, j, rgb);
             }
@@ -157,64 +184,27 @@ public class Painter : MonoBehaviour
 
         _map.Apply();
     }
-    
-    private Color HSVToRGB2(float intensity)
+
+    private static Color HSVToRGB(float intensity)
     {
-        var hue = 6 - intensity * 6;
+        var hue = 4 - intensity * 4;
         
-        var x = (1 - Mathf.Abs(hue % 2 - 1));
-
+        var x = 1 - Mathf.Abs(hue % 2 - 1);
+    
         var rgb = Color.clear;
-
+    
         if (hue == 0f) {
             rgb = new Color(1, 1, 1, 1);
         } else if (hue <= 1) {
             rgb = new Color(1, x, 0, 1);
         } else if (hue <= 2) {
             rgb = new Color(x, 1, 0, 1);
-        } else if (hue <= 6) {
+        } else if (hue <= 3) {
             rgb = new Color(0, 1, x, 1);
-        } else if (hue <= 6) {
+        } else if (hue <= 4) {
             rgb = new Color(0, x, 1, 1);
         }
-        // else if (hue <= 6) {
-        //     rgb = new Color(x, 0, 1, 1);
-        // }
-
+    
         return rgb;
     }
-
-    // private Color HSVToRGB(float intensity)
-    // {
-    //     var hue = 360 * intensity;
-    //     var r = ToColorCh(5, hue);
-    //     var g = ToColorCh(3, hue);
-    //     var b = ToColorCh(1, hue);
-    //     Debug.Log(hue + " " + r + " " + g + " " + b);
-    //     return new Color(r, g, b, 1);
-    // }
-    //
-    // private int K(int n, float hue)
-    // {
-    //     return Mathf.RoundToInt(n + hue / 60) % 6;
-    // }
-    //
-    // private int ToColorCh(int n, float hue)
-    // {
-    //     var k = K(n, hue);
-    //     return (1 - Mathf.Max(0, Mathf.Min(k, 4 - k, 1)));
-    // }
-    
-    // private void PaintUsingPosition(Component player)
-    // {
-    //     var pos = player.transform.position;
-    //     var x = (int) (Mathf.Abs(pos.x - halfW) * resW);
-    //     var z = (int) (Mathf.Abs(pos.z - halfH) * resH);
-    //
-    //     var color = _map.GetPixel(x, z);
-    //     color += new Color(0.001f, 0.001f, 0.001f);
-    //     _map.SetPixel(x, z, color);
-    //     
-    //     _map.Apply();
-    // }
 }
